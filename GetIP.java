cce339f401c9c80264057ff26ae2f7d9423d64da
Ankit Nagar <ankitnagar22091997@gmail.com>
2020-04-22 12:32:25 +0530

    cheat code of git tutorial

--- GetIP.java
+++
@@ -1,7 +0,0 @@
-import java.net.*;
-class GetIP{
-	public static void main(String[] args) throws Exception {
-		InetAddress mi=InetAddress.getLocalHost();
-		System.out.println("my ip address is : "+mi.getHostAddress());
-	}
-}
